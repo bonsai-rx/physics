c42bc6c1abce150d2a04d6b2b6b9decf54d03783
glopes <glopesdev@users.noreply.github.com>
2016-02-13 18:45:37 +0000

    Added support for specifying collision surface parameters.

---
+++ Bonsai.Physics/CollisionHandler.cs
@@ -0,0 +1,20 @@
+﻿using Ode.Net.Collision;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Physics
+{
+    public class CollisionHandler
+    {
+        public string Material1 { get; set; }
+
+        public string Material2 { get; set; }
+
+        [TypeConverter(typeof(SurfaceParametersConverter))]
+        public SurfaceParameters CollisionSurface { get; set; }
+    }
+}
