4110c4fd042871c1a92915d490e9322b6b7e23ff
glopes <glopes@localhost>
2016-02-17 13:46:53 +0000

    Added sphere geometry and mass constructors. Added joint feedback constructor.

---
+++ Bonsai.Physics/CreateJointFeedback.cs
@@ -0,0 +1,25 @@
+﻿using Ode.Net;
+using Ode.Net.Joints;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Physics
+{
+    [Combinator]
+    public class CreateJointFeedback
+    {
+        public IObservable<JointFeedback> Process(IObservable<Joint> source)
+        {
+            return source.SelectMany(joint =>
+            {
+                var feedback = new JointFeedback();
+                joint.Feedback = feedback;
+                return Observable.Return(feedback).Concat(Observable.Never(feedback)).Finally(feedback.Dispose);
+            });
+        }
+    }
+}
---
+++ Bonsai.Physics/CreateSphereMass.cs
@@ -0,0 +1,22 @@
+﻿using Ode.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Physics
+{
+    public class CreateSphereMass : Combinator<Body, Body>
+    {
+        public double Radius { get; set; }
+
+        public double TotalMass { get; set; }
+
+        public override IObservable<Body> Process(IObservable<Body> source)
+        {
+            return source.Do(body => body.Mass = Mass.CreateSphereTotal(TotalMass, Radius));
+        }
+    }
+}
