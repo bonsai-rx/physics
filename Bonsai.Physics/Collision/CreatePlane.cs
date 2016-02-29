43b50d4914c4ecb0b4eae2f110bfad191a40f538
glopes <glopesdev@users.noreply.github.com>
2016-02-29 01:42:40 +0000

    Added plane collision geometry and 2D plane joint constraint.

---
+++ Bonsai.Physics/Collision/CreatePlane.cs
@@ -0,0 +1,27 @@
+﻿using Ode.Net;
+using Ode.Net.Collision;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Physics.Collision
+{
+    public class CreatePlane : CreateGeom<Plane>
+    {
+        public double A { get; set; }
+
+        public double B { get; set; }
+
+        public double C { get; set; }
+
+        public double D { get; set; }
+
+        protected override Plane CreateGeometryObject(Space space)
+        {
+            return new Plane(space, A, B, C, D);
+        }
+    }
+}
---
+++ Bonsai.Physics/Joints/CreatePlane2D.cs
@@ -0,0 +1,25 @@
+﻿using Ode.Net;
+using Ode.Net.Joints;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Physics.Joints
+{
+    [Combinator]
+    public class CreatePlane2D
+    {
+        public IObservable<Plane2D> Process(IObservable<Body> source)
+        {
+            return source.SelectMany(body =>
+            {
+                var plane = new Plane2D(body.World);
+                plane.Attach(body);
+                return Observable.Return(plane).Concat(Observable.Never(plane)).Finally(plane.Dispose);
+            });
+        }
+    }
+}
