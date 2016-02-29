9d9046b3085a0a81a9de62e05abfb62187f7c241
glopes <glopes@localhost>
2016-02-29 01:44:50 +0000

    Added cylinder geometry and mass constructors.

---
+++ Bonsai.Physics/Collision/CreateCylinder.cs
@@ -0,0 +1,23 @@
+﻿using Ode.Net;
+using Ode.Net.Collision;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Physics.Collision
+{
+    public class CreateCylinder : CreateGeom<Cylinder>
+    {
+        public double Radius { get; set; }
+
+        public double Length { get; set; }
+
+        protected override Cylinder CreateGeometryObject(Space space)
+        {
+            return new Cylinder(space, Radius, Length);
+        }
+    }
+}
