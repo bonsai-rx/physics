0b8c236dcce6642a20ccc709dc590a3182bf1091
glopes <glopes@localhost>
2016-03-09 11:35:57 +0000

    Added triangle mesh geometry constructor.

---
+++ Bonsai.Physics/Collision/CreateTriMesh.cs
@@ -0,0 +1,28 @@
+﻿using Ode.Net.Collision;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Bonsai.Physics.Collision
+{
+    public class CreateTriMesh : CreateGeom<TriMesh>
+    {
+        [XmlIgnore]
+        public TriMeshData Data { get; set; }
+
+        protected override TriMesh CreateGeometryObject(Space space)
+        {
+            var data = Data;
+            if (data == null)
+            {
+                throw new InvalidOperationException("Valid triangle mesh data must be provided at creation time.");
+            }
+
+            return new TriMesh(space, data);
+        }
+    }
+}
