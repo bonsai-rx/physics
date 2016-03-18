7ea21e46f5c157ad3d61264fff4c01a70caeec2a
glopes <glopes@localhost>
2016-03-18 01:29:39 +0000

    Added heightfield geometry constructor.

---
+++ Bonsai.Physics/Collision/CreateHeightfield.cs
@@ -0,0 +1,29 @@
+﻿using Ode.Net.Collision;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Bonsai.Physics.Collision
+{
+    public class CreateHeightfield : CreateGeom<Heightfield>
+    {
+        [XmlIgnore]
+        public HeightfieldData Data { get; set; }
+
+        public bool Placeable { get; set; }
+
+        protected override Heightfield CreateGeometryObject(Space space)
+        {
+            var data = Data;
+            if (data == null)
+            {
+                throw new InvalidOperationException("Valid heightfield data must be provided at creation time.");
+            }
+
+            return new Heightfield(space, data, Placeable);
+        }
+    }
+}
