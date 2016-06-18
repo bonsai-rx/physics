4c1b1f78a0f76eb4c71d933dfcda65ffe0dbd67c
glopes <glopes@localhost>
2016-06-18 21:37:39 +0900

    Added trimesh mass constructor.

---
+++ Bonsai.Physics/CreateTriMeshMass.cs
@@ -0,0 +1,30 @@
+﻿using Ode.Net;
+using Ode.Net.Collision;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Physics
+{
+    public class CreateTriMeshMass : Combinator<TriMesh, TriMesh>
+    {
+        public double TotalMass { get; set; }
+
+        public override IObservable<TriMesh> Process(IObservable<TriMesh> source)
+        {
+            return source.Do(triMesh =>
+            {
+                var body = triMesh.Body;
+                if (body == null)
+                {
+                    throw new InvalidOperationException("The triangle mesh must be associated with a rigid body.");
+                }
+
+                body.Mass = Mass.CreateTriMeshTotal(TotalMass, triMesh);
+            });
+        }
+    }
+}
