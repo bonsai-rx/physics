c2df70e3ef97f27cf5251cc2bbb86e05a07ded9f
glopes <glopes@localhost>
2016-06-18 21:36:13 +0900

    Added AxisAnchor class and constructor node.

---
+++ Bonsai.Physics/AxisAnchor.cs
@@ -0,0 +1,30 @@
+﻿using Ode.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Physics
+{
+    public class AxisAnchor
+    {
+        public AxisAnchor(Vector3 axis, Vector3 anchor)
+        {
+            Axis = axis;
+            Anchor = anchor;
+        }
+
+        public AxisAnchor(
+            float axisX, float axisY, float axisZ,
+            float anchorX, float anchorY, float anchorZ)
+            : this(new Vector3(axisX, axisY, axisZ),
+                   new Vector3(anchorX, anchorY, anchorZ))
+        {
+        }
+
+        public Vector3 Axis { get; private set; }
+
+        public Vector3 Anchor { get; private set; }
+    }
+}
---
+++ Bonsai.Physics/CreateAxisAnchor.cs
@@ -0,0 +1,47 @@
+﻿using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Physics
+{
+    public class CreateAxisAnchor : Transform<Matrix4, AxisAnchor>
+    {
+        Vector3 initialAxis;
+        Vector3 initialAnchor;
+
+        public CreateAxisAnchor()
+        {
+            Axis = Ode.Net.Vector3.UnitX;
+        }
+
+        [TypeConverter(typeof(NumericAggregateConverter))]
+        public Ode.Net.Vector3 Axis
+        {
+            get { return new Ode.Net.Vector3(initialAxis.X, initialAxis.Y, initialAxis.Z); }
+            set { initialAxis = new Vector3((float)value.X, (float)value.Y, (float)value.Z); }
+        }
+
+        [TypeConverter(typeof(NumericAggregateConverter))]
+        public Ode.Net.Vector3 Anchor
+        {
+            get { return new Ode.Net.Vector3(initialAnchor.X, initialAnchor.Y, initialAnchor.Z); }
+            set { initialAnchor = new Vector3((float)value.X, (float)value.Y, (float)value.Z); }
+        }
+
+        public override IObservable<AxisAnchor> Process(IObservable<OpenTK.Matrix4> source)
+        {
+            return source.Select(input =>
+            {
+                Vector3 axis, anchor;
+                Vector3.TransformNormal(ref initialAxis, ref input, out axis);
+                Vector3.TransformPosition(ref initialAnchor, ref input, out anchor);
+                return new AxisAnchor(axis.X, axis.Y, axis.Z, anchor.X, anchor.Y, anchor.Z);
+            });
+        }
+    }
+}
