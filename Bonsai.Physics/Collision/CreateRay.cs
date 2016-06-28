2fd39c37e8840834b0d4be7cabb2db40570ae1d1
glopes <glopesdev@users.noreply.github.com>
2016-06-28 11:22:38 +0900

    Added constructor for ray geometries.

---
+++ Bonsai.Physics/Collision/CreateRay.cs
@@ -0,0 +1,21 @@
+﻿using Ode.Net;
+using Ode.Net.Collision;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Physics.Collision
+{
+    public class CreateRay : CreateGeom<Ray>
+    {
+        public double Length { get; set; }
+
+        protected override Ray CreateGeometryObject(Space space)
+        {
+            return new Ray(space, Length);
+        }
+    }
+}
