47d78f1caa6f4f83d7b5a3b1d45b250c7b76d1a5
glopes <glopes@localhost>
2016-07-28 12:57:05 +0900

    Added converter node to extract a full transform matrix from a Geom object.

---
+++ Bonsai.Physics/GeomTransform.cs
@@ -0,0 +1,28 @@
+﻿using Ode.Net;
+using Ode.Net.Collision;
+using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Physics
+{
+    public class GeomTransform : Combinator<Geom, Matrix4>
+    {
+        public override IObservable<Matrix4> Process(IObservable<Geom> source)
+        {
+            return source.Select(geom =>
+            {
+                Ode.Net.Vector3 position;
+                Ode.Net.Quaternion orientation;
+                geom.GetPosition(out position);
+                geom.GetQuaternion(out orientation);
+                return Matrix4.CreateFromQuaternion(new OpenTK.Quaternion((float)orientation.X, (float)orientation.Y, (float)orientation.Z, (float)orientation.W)) *
+                    Matrix4.CreateTranslation((float)position.X, (float)position.Y, (float)position.Z);
+            });
+        }
+    }
+}
