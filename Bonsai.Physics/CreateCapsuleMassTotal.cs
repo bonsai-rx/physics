c7f4b358e76186fd98c5a2ea161bb0f8da57a989
glopes <glopesdev@users.noreply.github.com>
2016-09-10 14:54:37 +0100

    Added capsule mass constructor.

---
+++ Bonsai.Physics/CreateCapsuleMassTotal.cs
@@ -0,0 +1,26 @@
+﻿using Ode.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Physics
+{
+    public class CreateCapsuleMassTotal : Combinator<Body, Body>
+    {
+        public double Radius { get; set; }
+
+        public double Length { get; set; }
+
+        public DirectionAxis Direction { get; set; }
+
+        public double TotalMass { get; set; }
+
+        public override IObservable<Body> Process(IObservable<Body> source)
+        {
+            return source.Do(body => body.Mass = Mass.CreateCapsuleTotal(TotalMass, Direction, Radius, Length));
+        }
+    }
+}
