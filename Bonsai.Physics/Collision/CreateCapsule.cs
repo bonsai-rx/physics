5daca57260fd08902c1d144996d36caca5f24904
glopes <glopes@localhost>
2016-09-10 14:51:33 +0100

    Added constructor for capsule geometries.

---
+++ Bonsai.Physics/Collision/CreateCapsule.cs
@@ -0,0 +1,23 @@
+﻿using Ode.Net;
+using Ode.Net.Collision;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Physics.Collision
+{
+    public class CreateCapsule : CreateGeom<Capsule>
+    {
+        public double Radius { get; set; }
+
+        public double Length { get; set; }
+
+        protected override Capsule CreateGeometryObject(Space space)
+        {
+            return new Capsule(space, Radius, Length);
+        }
+    }
+}
