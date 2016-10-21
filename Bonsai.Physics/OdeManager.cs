e3e16e7ec55f0b26e72689670a875c33e61ea9cd
glopes <glopesdev@users.noreply.github.com>
2016-10-21 15:10:50 +0100

    Added shared automatic management of ODE engine initialization.

---
+++ Bonsai.Physics/OdeManager.cs
@@ -0,0 +1,37 @@
+﻿using Ode.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Physics
+{
+    static class OdeManager
+    {
+        static int counter;
+        static readonly object gate = new object();
+
+        internal static IDisposable ReserveEngine()
+        {
+            lock (gate)
+            {
+                if (counter++ == 0)
+                {
+                    Engine.Init();
+                }
+            }
+            return Disposable.Create(() =>
+            {
+                lock (gate)
+                {
+                    if (--counter == 0)
+                    {
+                        Engine.Close();
+                    }
+                }
+            });
+        }
+    }
+}
