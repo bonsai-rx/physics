35cb3f4c86bc97902be12f43cdcd58c4f87c1eb7
glopes <glopes@localhost>
2016-10-21 15:42:44 +0100

    Added AddTorque module.

---
+++ Bonsai.Physics/AddTorque.cs
@@ -0,0 +1,26 @@
+﻿using Ode.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Physics
+{
+    public class AddTorque : Sink<Body>
+    {
+        public Vector3 Torque { get; set; }
+
+        public bool Relative { get; set; }
+
+        public override IObservable<Body> Process(IObservable<Body> source)
+        {
+            return source.Do(body =>
+            {
+                if (Relative) body.AddRelativeTorque(Torque);
+                else body.AddTorque(Torque);
+            });
+        }
+    }
+}
