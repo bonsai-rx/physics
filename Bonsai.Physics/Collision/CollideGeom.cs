807082ad963351ab1ebc014b0b438dc0e9c410c6
glopes <glopesdev@users.noreply.github.com>
2018-02-13 01:03:59 +0000

    Added CollideGeom and CollideSpace operators for collision detection.

---
+++ Bonsai.Physics/Collision/CollideGeom.cs
@@ -0,0 +1,36 @@
+﻿using Ode.Net.Collision;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Physics.Collision
+{
+    [Description("Computes contact information between geometries that potentially intersect.")]
+    public class CollideGeom : Transform<Geom, ContactGeom[]>
+    {
+        [Description("The maximum number of contacts generated per collision pair.")]
+        public int MaxContacts { get; set; }
+
+        static ContactGeom[] Collide(Geom geom1, Geom geom2, int maxContacts)
+        {
+            var contacts = new ContactGeom[maxContacts];
+            var count = Geom.Collide(geom1, geom2, contacts);
+            Array.Resize(ref contacts, count);
+            return contacts;
+        }
+
+        public override IObservable<ContactGeom[]> Process(IObservable<Geom> source)
+        {
+            return source.Select(geom => Collide(geom, geom.Space, MaxContacts));
+        }
+
+        public IObservable<ContactGeom[]> Process(IObservable<Tuple<Geom, Geom>> source)
+        {
+            return source.Select(input => Collide(input.Item1, input.Item2, MaxContacts));
+        }
+    }
+}
