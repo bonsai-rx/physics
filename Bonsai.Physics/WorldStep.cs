9f2c7c6b48995bbfa8c6a62695a635c7e2e1b20f
glopes <glopes@localhost>
2018-02-13 01:05:03 +0000

    Added WorldStep operator for advancing the state of a dynamics simulation.

---
+++ Bonsai.Physics/WorldStep.cs
@@ -0,0 +1,23 @@
+﻿using Ode.Net;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Physics
+{
+    [Description("Advances the dynamics simulation by the specified time delta.")]
+    public class WorldStep : Sink<World>
+    {
+        [Description("The number of seconds the simulation should advance in each step.")]
+        public double StepSize { get; set; }
+
+        public override IObservable<World> Process(IObservable<World> source)
+        {
+            return source.Do(world => world.QuickStep(StepSize));
+        }
+    }
+}
